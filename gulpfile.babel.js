c80ecf8455bd28874ea33435f964f181de477f89
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2018-08-11 15:56:12 -0400

    Improve

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,12 @@
 import autoPrefixer from 'gulp-autoprefixer';
 import babel from 'gulp-babel';
 import cssNano from 'gulp-cssnano';
+import debug from 'gulp-debug';
 import del from 'del';
 import esLint from 'gulp-eslint';
 import gulp from 'gulp';
+import less from 'gulp-less';
+import mergeStream from 'merge-stream';
 import mustache from 'gulp-mustache';
 import rename from 'gulp-rename';
 import sass from 'gulp-sass';
@@ -16,10 +19,62 @@ export function clean() {
 	return del('./dist');
 }
 
-const buildJsSrc = './src/**/*.js';
+////////////////////////////////////////////////////////////////////////////////
+// HTML
+////////////////////////////////////////////////////////////////////////////////
+
+const buildHtmlSrc = './src/**/!(*.template).html';
+export function buildHtml() {
+	const buildHtmlDest = './dist/';
+	return gulp.src(buildHtmlSrc)
+		.pipe(debug())
+		.pipe(mustache())
+		.pipe(webDependencies({ dest: buildHtmlDest, prefix: './vendors/' }))
+		.pipe(gulp.dest(buildHtmlDest));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CSS
+////////////////////////////////////////////////////////////////////////////////
+
+const buildCssSrc = './src/**/!(*.template).css';
+const buildLessSrc = './src/**/!(*.template).less';
+const buildSassSrc = './src/**/!(*.template).scss';
+export function buildCss() {
+	const cssStream = gulp.src(buildCssSrc)
+		.pipe(debug())
+		.pipe(mustache());
+
+	const lessStream = gulp.src(buildLessSrc)
+		.pipe(debug())
+		.pipe(mustache())
+		.pipe(less());
+
+	const scssStream = gulp.src(buildSassSrc)
+		.pipe(debug())
+		.pipe(mustache())
+		.pipe(sass());
+
+	const buildCssDest = './dist/';
+	return mergeStream(cssStream, lessStream, scssStream)
+		.pipe(autoPrefixer())
+		.pipe(gulp.dest(buildCssDest))
+		.pipe(rename((path) => path.basename += '.min'))
+		.pipe(sourceMaps.init())
+		.pipe(cssNano())
+		.pipe(sourceMaps.write('.'))
+		.pipe(gulp.dest(buildCssDest));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// SCRIPT
+////////////////////////////////////////////////////////////////////////////////
+
+const buildJsSrc = './src/**/!(*.template).js';
 export function buildJs() {
 	const buildJsDest = './dist/';
 	return gulp.src(buildJsSrc)
+		.pipe(debug())
 		.pipe(mustache())
 		.pipe(esLint())
 		.pipe(esLint.format())
@@ -32,32 +87,11 @@ export function buildJs() {
 		.pipe(gulp.dest(buildJsDest));
 }
 
-const buildSassSrc = './src/**/*.scss';
-export function buildSass() {
-	const buildSassDest = './dist/';
-	return gulp.src(buildSassSrc)
-		.pipe(mustache())
-		.pipe(sass())
-		.pipe(autoPrefixer())
-		.pipe(gulp.dest(buildSassDest))
-		.pipe(rename((path) => path.basename += '.min'))
-		.pipe(sourceMaps.init())
-		.pipe(cssNano())
-		.pipe(sourceMaps.write('.'))
-		.pipe(gulp.dest(buildSassDest));
-}
+////////////////////////////////////////////////////////////////////////////////
+// BUILD
+////////////////////////////////////////////////////////////////////////////////
 
-const buildHtmlSrc = './src/**/*.html';
-export function buildHtml() {
-	const buildHtmlDest = './dist/';
-	return gulp.src(buildHtmlSrc)
-		.pipe(mustache())
-		.pipe(webDependencies({ dest: buildHtmlDest, prefix: './vendors/' }))
-		.pipe(gulp.dest(buildHtmlDest));
-}
-
-export const build = gulp.series(clean, gulp.parallel(buildJs, buildSass, buildHtml));
-export default build;
+export const build = gulp.series(clean, gulp.parallel(buildJs, buildCss, buildHtml));
 
 export function watch() {
 	const servePath = '.';
@@ -65,14 +99,32 @@ export function watch() {
 		.pipe(webServer({
 			directoryListing: { enable: true, path: servePath },
 			livereload: true,
-			open: true
+			open: true,
+			port: 8080
 		}));
 
-	gulp.watch(buildJsSrc, buildJs);
-	gulp.watch(buildSassSrc, buildSass);
-	gulp.watch(buildHtmlSrc, buildHtml);
+	const htmlTemplate = './src/**/*.template.html';
+	gulp.watch((Array.isArray(buildHtmlSrc) ? buildHtmlSrc : [buildHtmlSrc]).concat(
+		Array.isArray(htmlTemplate) ? htmlTemplate : [htmlTemplate]
+	), buildHtml);
 
-	const templateGlob = './src/**/*.template.html';
-	gulp.watch(templateGlob, build);
+	const cssTemplate = './src/**/*.template.css';
+	const lessTemplate = './src/**/*.template.less';
+	const scssTemplate = './src/**/*.template.scss';
+	gulp.watch((Array.isArray(buildCssSrc) ? buildCssSrc : [buildCssSrc]).concat(
+		Array.isArray(cssTemplate) ? cssTemplate : [cssTemplate],
+		Array.isArray(buildLessSrc) ? buildLessSrc : [buildLessSrc],
+		Array.isArray(lessTemplate) ? lessTemplate : [lessTemplate],
+		Array.isArray(buildSassSrc) ? buildSassSrc : [buildSassSrc],
+		Array.isArray(scssTemplate) ? scssTemplate : [scssTemplate]
+	), buildCss);
+
+	const jsTemplate = './src/**/*.template.js';
+	gulp.watch((Array.isArray(buildJsSrc) ? buildJsSrc : [buildJsSrc]).concat(
+		Array.isArray(jsTemplate) ? jsTemplate : [jsTemplate]
+	), buildJs);
 }
+
 export const serve = gulp.series(build, watch);
+
+export default build;
